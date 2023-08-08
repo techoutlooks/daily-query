442059672736e3bcf6f7876d29107e8f3dce5ff1
Edouard C <ceduth@techoutlooks.com>
2023-08-08 22:58:25 +0000

    fix: replace `strptime` by `fromisoformat` more resilent

--- src/daily_query/helpers.py
+++ src/daily_query/helpers.py
@@ -33,7 +33,7 @@ def mk_datetime(input=None, date_only=False) -> datetime.datetime:
         if len(date_time.split()) == 1:
             date_time = f"{str(date_time)} 00:00:00"
         date_time = str(date_time).split('.')[0]
-        date_time = datetime.datetime.strptime(date_time, DATETIME_FORMAT)
+        date_time = datetime.datetime.fromisoformat(date_time)
     if date_only:
         date_time = date_time.date()
     return date_time
